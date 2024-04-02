1a5b2297591a0e40d59d3a320150dee419ba0d19
Kevin <kevinsmith2001233@gmail.com>
2024-04-02 16:55:12 +0800

    Shop 4月2日

--- src/main/java/com/example/shop/config/MyMVCConfig.java
+++ src/main/java/com/example/shop/config/MyMVCConfig.java
@@ -1,6 +1,7 @@
 package com.example.shop.config;
 
 import com.example.shop.interceptor.Interceptor1;
+import com.example.shop.interceptor.Interceptor2;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Configurable;
 import org.springframework.context.annotation.Configuration;
@@ -14,17 +15,19 @@ public class MyMVCConfig implements WebMvcConfigurer {
     @Override
     public void addViewControllers (ViewControllerRegistry registry){
         registry.addViewController("/home").setViewName("home");
+        registry.addViewController("/login").setViewName("login");
+        registry.addViewController("/loginFailed").setViewName("loginFailed");
     }
 @Autowired
     private Interceptor1 interceptor1;
-//    @Autowired
-//    private Interceptor2 interceptor2;
-//    @Override
+    @Autowired
+    private Interceptor2 interceptor2;
+    @Override
     public void addInterceptors(InterceptorRegistry registry){
         registry.addInterceptor(interceptor1)
                 .addPathPatterns("/home");
-        //registry.addInterceptor(interceptor2)
-        //.addPathPatterns("/home");
+        registry.addInterceptor(interceptor2)
+                .addPathPatterns("/home");
 
     }
 }
--- src/main/java/com/example/shop/interceptor/Interceptor2.java
+++ src/main/java/com/example/shop/interceptor/Interceptor2.java
@@ -1,4 +1,35 @@
 package com.example.shop.interceptor;
+import org.springframework.stereotype.Component;
+import org.springframework.web.servlet.HandlerInterceptor;
+import org.springframework.web.servlet.ModelAndView;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+    @Component
 
-public class Interceptor2 {
-}
+    public class Interceptor2 implements HandlerInterceptor {
+        @Override
+        public boolean preHandle(HttpServletRequest request,
+                                 HttpServletResponse response, Object handler) throws Exception{
+            System.out.println("Interceptor2======before execution");
+            String userName=request.getParameter("userName");
+            String passWord=request.getParameter("passWord");
+            if(userName != null && userName.equals("Kevin")
+            && passWord != null && passWord.equals("123")){
+                return true;
+            }
+            response.sendRedirect("/loginFailed");
+            return  false;
+        }
+        @Override
+        public void postHandle(HttpServletRequest request,
+                               HttpServletResponse response,Object handler,
+                               ModelAndView modelAndView) throws Exception{
+            System.out.println("Interceptor2======execution");
+        }
+        @Override
+        public void afterCompletion(HttpServletRequest request,
+                                    HttpServletResponse response,Object handler,
+                                    Exception ex) throws Exception{
+            System.out.println("Interceptor2======after execution");
+        }
+    }
